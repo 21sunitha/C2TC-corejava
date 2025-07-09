a5c822f418823a2b0b9a5fbb5fb5d74680dedc57
sunit <sunit@SUNITHADEVEN>
2025-07-09 11:06:23 +0530

    update

---
+++ C2TC_corejavaprograms/src/Day14InterThread/Producer.java
@@ -0,0 +1,34 @@
+package Day14InterThread;
+
+public class Producer {
+	Q obj;
+
+	public Producer(Q obj) {
+
+		this.obj = obj;
+		start();
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void run() {
+		int i = 0;
+		while (true) {
+			try {
+				Thread.sleep(3000);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			obj.put(i);
+			i++;
+		}
+	}
+
+	public void join() {
+		// TODO Auto-generated method stub
+		
+	}
+}
